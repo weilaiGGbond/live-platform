21889db8e36ead061fd8bc4862530d32d56fcc11
CraneReturn <1066063143@qq.com>
2024-04-18 10:09:19 +0800

    引入图标

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import './assets/main.css'
-
+import '@/assets/style/variables.scss'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'animate.css';
 import router from './router/index'
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(router).use(ElementPlus).mount('#app')
